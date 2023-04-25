c15996ed36ffacd514010f7c9af8737bf8a7b2fc
adrianabedon <abedon.adrian@gmail.com>
2023-04-25 16:52:54 -0400

    transpile if statements

--- python_transpiler/samples/conditions.sql
+++ python_transpiler/samples/conditions.sql
@@ -1,12 +1,17 @@
 CREATE FUNCTION if_func(cid integer) RETURNS VARCHAR AS $$
 DECLARE pd VARCHAR = 'function'; 
 DECLARE pd2 VARCHAR;
+declare i integer = 0;
 BEGIN
     pd2 := pd+'test'+pd+pd2;
     if cid < 3 then 
         cid := cid + 1;
+    elsif cid = 3 then
+    elsif cid > 5 then
+        cid := cid * 2;
     else
         cid := cid - 1;
+        cid := cid + 1;
     end if;
     RETURN concat(cid,pd2);
 END; $$
